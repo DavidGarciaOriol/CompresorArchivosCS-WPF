48a948aae539bda86e1fd042c5423ccf78039e02
ciclot <ciclot@A2.11_PC20>
2023-10-25 21:59:21 +0200

    - - -

--- CompresorArchivosWPF/MainWindow.xaml.cs
+++ CompresorArchivosWPF/MainWindow.xaml.cs
@@ -16,17 +16,17 @@ namespace CompresorArchivosWPF
     public partial class MainWindow : Window
     {
         // Lista de imágenes recientes.
-        private List<string> imagePaths = new List<string>();
+        private List<string> filePaths = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
             AllowDrop = true;
-            Drop += ImageDrop; // Se llama al método al soltar un archivo en la ventana. Se puede hacer así o poner el enlace al método del evento en el componente directamente en el XAML. Lo destaco porque me he fijado en ello a raíz de este proyecto.
+            Drop += FileDrop;
         }
 
         // Se añade la imagen dropeada en el drop-área a la lista de recientes y se muestra en el marco.
-        private void ImageDrop(object sender, DragEventArgs e)
+        private void FileDrop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
@@ -34,12 +34,9 @@ namespace CompresorArchivosWPF
 
                 foreach (string file in files)
                 {
-                    if (IsImageFile(file))
-                    {
-                        imagePaths.Add(file); // Se añade a la lista de rutas de recientes.
-                        AddRecentImageMenuItem(file); // Añade un nuevo componente a la lista de recientes que contendrá la ruta de la nueva imagen reciente.
-                        CompressFile(file); // La imagen se muestra en el visualizador.
-                    }
+                    filePaths.Add(file); // Se añade a la lista de rutas de recientes.
+                    AddRecentImageMenuItem(file); // Añade un nuevo componente a la lista de recientes que contendrá la ruta de la nueva imagen reciente.
+                    CompressFile(file); // La imagen se muestra en el visualizador.
                 }
             }
         }
@@ -47,35 +44,30 @@ namespace CompresorArchivosWPF
         // Muestra la imagen recibida por parámetros en forma de path en el visualizador.
         private void CompressFile(string directoryPath)
         {
-            if (Directory.Exists(directoryPath))
-            {
+            // TODO
 
+            CommonDialog compressYesOrNoneDialog;
+            
+            if (true)
+            {
+                // TODO
             }
         }
 
-        // Se comprueba que el archivo dropeado sea un archivo de imagen.
-        private bool IsImageFile(string filePath)
-        {
-            string extension = System.IO.Path.GetExtension(filePath).ToLower();
-            return extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif" || extension == ".bmp" || extension == ".webp";
-        }
-
         // Método que abre la imagen reciente al clickar en el componente con la ruta de la imagen añadida anteriormente.
-        private void OpenImage_Click(object sender, RoutedEventArgs e)
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
-            string imagePath = ((MenuItem)sender).Tag.ToString();
-            DisplayImage(imagePath);
+            string filePath = ((MenuItem)sender).Tag.ToString();
+            
         }
 
-        // Método que confecciona dinámicamente y añade componentes de path recientes al menú de recientes.
         private void AddRecentImageMenuItem(string filePath)
         {
             MenuItem recentFileMenuItem = new MenuItem();
             recentFileMenuItem.Header = System.IO.Path.GetFileName(filePath);
             recentFileMenuItem.Tag = filePath;
-            recentFileMenuItem.Click += OpenImage_Click; // Se enlaza el evento al componente de forma dinámica por código.
+            recentFileMenuItem.Click += OpenFile_Click;
 
-            // Agregar el elemento al submenú "Recientes". Si sobrepasa los 20 elementos, borra el más antiguo.
             if (menuRecientes.Items.Count >= 20)
             {
                 menuRecientes.Items.RemoveAt(19);
